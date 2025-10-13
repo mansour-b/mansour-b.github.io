f64c4b7cd41e68e71cc42a88181ad1764b671f1a
Mansour Benbakoura <mansour.benbakoura@inria.fr>
2025-10-13 16:34:05 +0200

    random dynamic url

---
+++ _plugins/digest_filter.rb
@@ -0,0 +1,20 @@
+require 'digest'
+
+module Jekyll
+  module DigestFilter
+    def digest(input, algorithm = 'MD5')
+      case algorithm.upcase
+      when 'MD5'
+        Digest::MD5.hexdigest(input)
+      when 'SHA1'
+        Digest::SHA1.hexdigest(input)
+      when 'SHA256'
+        Digest::SHA256.hexdigest(input)
+      else
+        raise "Unknown digest algorithm: #{algorithm}"
+      end
+    end
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::DigestFilter)
