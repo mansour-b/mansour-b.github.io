d22751f654edd042fa93134ac929432046c4c5ad
Mansour Benbakoura <mansour.benbakoura@inria.fr>
2025-10-13 17:43:26 +0200

    try to fire plugin earlier

--- _plugins/digest_filter.rb
+++ _plugins/digest_filter.rb
@@ -20,12 +20,11 @@ end
 Liquid::Template.register_filter(Jekyll::DigestFilter)
 
 # --- Give the "Random Stuff" page a daily hashed permalink
-Jekyll::Hooks.register :pages, :pre_render do |page, payload|
-  # match either by title (case-insensitive) or filename containing 'random'
+Jekyll::Hooks.register :pages, :post_init do |page|
   if page.data['title']&.downcase == 'random stuff' || page.path.include?('random')
     today = Time.now.utc.strftime('%Y-%m-%d')
     hash  = Digest::MD5.hexdigest(today)[0,8]
     page.data['permalink'] = "/random-#{hash}/"
-    Jekyll.logger.info "✨ Random Stuff permalink set to:", page.data['permalink']
+    Jekyll.logger.info "✨ Random Stuff permalink (post_init) set to:", page.data['permalink']
   end
 end
